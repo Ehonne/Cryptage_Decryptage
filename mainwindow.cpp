4f47c1aa229cb4ed584a079b8c2036e04f067240
Ehonne <63787767+Ehonne@users.noreply.github.com>
2020-11-12 16:26:24 +0100

    Creation d'une fonction affiche_arbre pour raison de clarté

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -38,26 +38,16 @@ int getLevel(Sommet *node, int data)
     return getLevelUtil(node, data, 1);
 }
 
-void MainWindow::paintEvent(QPaintEvent *event)
+void MainWindow::affiche_arbre(ArbreB B)
 {
 
-    setFixedSize(500, 500);
-    QPainter painter(this);
 
     int etage_2 = 100;
     int etage_3 = etage_2 -50;
-
-    ArbreB B;
-    B.Ajouter(5);
-    B.Ajouter(8);
-    B.Ajouter(2);
-    B.Ajouter(1);
-    B.Ajouter(10);
-    B.Ajouter(6);
-    B.Affiche(B.racine,0);
-
-    int cpt = 1; // compteur pour connaitre l'index du sommet
-    int cpp = 1;
+    QPainter painter(this);
+    int cpt2 = 1; // compteur du nombre de sommet par etage;
+    int cpt3 = 1;
+    int cpt4 = 1;
     queue<Sommet*> fifo;      // Création d'une file
     fifo.push(B.racine);          // Mise de la racine dans la file
     while(!fifo.empty())
@@ -79,7 +69,7 @@ void MainWindow::paintEvent(QPaintEvent *event)
 
              case 2: {      //Profondeur 2
 
-                if(cpp == 1)
+                if(cpt2 == 1)
                 {
                     if(n->valeur == 22222)
                     {
@@ -96,7 +86,7 @@ void MainWindow::paintEvent(QPaintEvent *event)
                     etage_2 += 200;
                     }
                 }
-                if(cpp == 2)
+                if(cpt2 == 2)
                 {
                     if(n->valeur == 22222)
                     {
@@ -118,7 +108,7 @@ void MainWindow::paintEvent(QPaintEvent *event)
 
              case 3: {      //Profondeur 3
 
-                if (cpt == 1)
+                if (cpt3 == 1)
                 {
                     if(n->valeur == 33333)
                     {
@@ -134,7 +124,7 @@ void MainWindow::paintEvent(QPaintEvent *event)
                         etage_3 += 100;
                     }
                 }
-                if (cpt == 2)
+                if (cpt3 == 2)
                 {
                     if(n->valeur == 33333)
                     {
@@ -150,7 +140,7 @@ void MainWindow::paintEvent(QPaintEvent *event)
                         etage_3 += 100;
                     }
                 }
-                if (cpt == 3)
+                if (cpt3 == 3)
                 {
                     if(n->valeur == 33333)
                     {
@@ -166,7 +156,7 @@ void MainWindow::paintEvent(QPaintEvent *event)
                         etage_3 += 100;
                     }
                 }
-                if (cpt == 4)
+                if (cpt3 == 4)
                 {
                     if(n->valeur == 33333)
                     {
@@ -183,36 +173,44 @@ void MainWindow::paintEvent(QPaintEvent *event)
                     }
 
                 }
-                cpt++;
-
-                /*if(cpt < 2) //fils gauche
-                {
-                    painter.drawLine(etage_2+25-400,200,etage_3+25,275);
-                    const QRect rectangle = QRect(etage_3, 250, 50, 50);
-                    painter.setBrush(Qt::red);
-                    painter.drawEllipse(etage_3, 250, 50, 50);
-                    painter.drawText(rectangle, Qt::AlignCenter, QString("%1").arg(n->valeur));
-                    etage_3 += 200;
-
-                    cpt++;
-
-                }
-                else    //fils droite
-                {
-                    painter.drawLine(etage_2+25-200,200,etage_3+25,275);
-                    const QRect rectangle = QRect(etage_3, 250, 50, 50);
-                    painter.setBrush(Qt::red);
-                    painter.drawEllipse(etage_3, 250, 50, 50);
-                    painter.drawText(rectangle, Qt::AlignCenter, QString("%1").arg(n->valeur));
-                    etage_3 += 100;
-                }*/
+                cpt3++;
                 break;
              }
         }
         if (n->fils_gauche != NULL) fifo.push(n->fils_gauche);       // Ajout du fils gauche s'il existe
-        if(((n->fils_droite == NULL) && (n->fils_gauche != NULL)) || ((n->fils_droite != NULL) && (n->fils_gauche == NULL)))    //SI on a 1 fils on le retiens
+
+        //Cas ou on a que le coter droite (suite a une suppretion du coter gauche par exemple)
+        //On rajoute 2 fantome a l'etage en amont
+        if(!fifo.empty())
         {
-            Sommet * vide = new Sommet;
+            Sommet* prob_sup = fifo.front();
+            if (((prob_sup->fils_droite != NULL) || (prob_sup->fils_gauche != NULL)) && ((n->fils_droite == NULL) && (n->fils_gauche == NULL)))
+            {
+                Sommet * vide = new Sommet;    Sommet* vide2 = new Sommet; //Sommet fantome
+                vide->fils_droite = NULL;       vide2->fils_droite = NULL;
+                vide->fils_gauche = NULL;       vide2->fils_gauche = NULL;
+                if(getLevel(B.racine, n->valeur) == 2)
+                {
+                    vide->valeur = 33333;       //Valeur aberante pour profondeur 2;
+                    vide2->valeur = 33333;
+                }
+                if(getLevel(B.racine, n->valeur) == 3)
+                {
+                    vide->valeur = 33333;       //Valeur aberante pour profondeur 3;
+                    vide2->valeur = 33333;
+                }
+                if(getLevel(B.racine, n->valeur) == 4)
+                {
+                    vide->valeur = 44444;       //Valeur aberante pour profondeur 4;
+                    vide2->valeur = 44444;
+                }
+                fifo.push(vide);
+                fifo.push(vide2);
+            }
+        }
+        if(((n->fils_droite == NULL) && (n->fils_gauche != NULL)) || ((n->fils_droite != NULL) && (n->fils_gauche == NULL)))    //SI on a 1 fils Creation d'un sommet fantom pour conserver l'espacement
+        {
+            Sommet * vide = new Sommet;     //Sommet fantome
             vide->fils_droite = NULL;
             vide->fils_gauche = NULL;
 
@@ -235,12 +233,36 @@ void MainWindow::paintEvent(QPaintEvent *event)
                     vide->valeur = 44444;       //Valeur aberante pour profondeur 4;
             }
 
-
             fifo.push(vide);
         }
         if (n->fils_droite != NULL) fifo.push(n->fils_droite);       // Ajout du fils droit s'il existe
 
     }
+}
+
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+
+    setFixedSize(500, 500);
+
+
+    ArbreB B;
+    B.Ajouter(5);
+    B.Ajouter(8);
+    B.Ajouter(2);
+    B.Ajouter(1);
+    B.Ajouter(10);
+    B.Ajouter(6);
+    B.Affiche(B.racine,0);
+
+    //affiche_arbre(B);
+
+    Sommet * ptr = B.Recherche(1);
+
+    B.Supprimer(ptr);
+    B.Affiche(B.racine,0);
+    affiche_arbre(B);
 
 
 }
--- mainwindow.h
+++ mainwindow.h
@@ -11,6 +11,7 @@ class MainWindow : public QWidget {
 
     protected:
         void paintEvent(QPaintEvent* e);
+        void affiche_arbre(ArbreB B);
     signals:
 
     public slots:
