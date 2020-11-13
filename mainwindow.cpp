8eeb55aae121d18bde3d5a1e909e344e141655ea
Ehonne <63787767+Ehonne@users.noreply.github.com>
2020-11-13 11:47:08 +0100

    Fonction affichage pour arbre de profondeur max 4 + commentaire
    ATTENTION L'AFFICHAGE NE PREND PAS EN CHARGE LES ARBRES OU 2 VALEURS SONT EGALS

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 
 
-int getLevelUtil(Sommet *node,  int data, int level)    // Obtenir le niv d'un sommet
+int getLevelUtil(Sommet *node,  int data, int level)    // Obtenir l'etage d'un sommet
 {
     if (node == NULL)
         return 0;
@@ -29,77 +29,86 @@ int getLevelUtil(Sommet *node,  int data, int level)    // Obtenir le niv d'un s
 }
 int getLevel(Sommet *node, int data)
 {
-    if(data == 33333)
+    if(data == 33333) //Si on a un fantome d'etage 3
            return 3;
-    if(data == 22222)
+    if(data == 22222) //Si on a un fantome d'etage 2
            return 2;
-    if(data == 44444)
+    if(data == 44444) //Si on a un fantome d'etage 4
            return 4;
     return getLevelUtil(node, data, 1);
 }
 
+
+//On fait un parcour en largeur de l'arbre de facons a le dessiner etage par etage. Pour se faire on stoke la racine de l'arbre dans une pile.
+// puis des que l'on va depiler on va dessiner le sommet avec son lien a son pere, puis on va ajouter ses fils dans la pile
+// Enfin pour des raisons d'affichage on rajoute des sommet fantome (sans valeur ni fils).
+// ATTENTION L'AFFICHAGE NE PEUX PAS PRENDRE EN CHARGE DES ARBRES OU 2(ou +) VALEURS SONT EGALES
 void MainWindow::affiche_arbre(ArbreB B)
 {
+    int etage_2 = 125;
+    int etage_3 = etage_2 -75;
+    int etage_3_bis = 325;
+    int etage_4 = etage_3 - 37;
+    int etage_4_bis = 200-37;
+    int etage_4_bis_bis = etage_3_bis -37;
+    int etage_4_bis_bis_bis = 475-37;
 
-
-    int etage_2 = 100;
-    int etage_3 = etage_2 -50;
     QPainter painter(this);
-    int cpt2 = 1; // compteur du nombre de sommet par etage;
+    int cpt2 = 1;       // compteur du nombre de sommet par etage;
     int cpt3 = 1;
     int cpt4 = 1;
-    queue<Sommet*> fifo;      // Création d'une file
-    fifo.push(B.racine);          // Mise de la racine dans la file
+    queue<Sommet*> fifo;        // Création d'une file
+    fifo.push(B.racine);        // Mise de la racine dans la file
     while(!fifo.empty())
     {
         Sommet *n = fifo.front();       // Nouveau sommet à traiter en tête de file
-        fifo.pop();
+        fifo.pop();                     //On supprime de la file le sommet que l'on va traiter
 
         //traitement du sommet :
-        cout << "Level of "<< n->valeur << " is " << getLevel(B.racine, n->valeur) << endl;
-        switch(getLevel(B.racine, n->valeur))
+        //cout << "Level of "<< n->valeur << " is " << getLevel(B.racine, n->valeur) << endl;
+        switch(getLevel(B.racine, n->valeur))   //Derterminer a quel etage de l'arbre on se trouve (profondeur)
         {
             case 1: {       //Profondeur 1
-                const QRect rectangle = QRect(200, 50, 50, 50);
-                painter.setBrush(Qt::red);
-                painter.drawEllipse(200, 50, 50, 50);
-                painter.drawText(rectangle, Qt::AlignCenter, QString("%1").arg(n->valeur));
+                const QRect rectangle = QRect(265, 25, 50, 50);
+                painter.setBrush(Qt::red);  //Coloration en rouge
+                painter.drawEllipse(265, 25, 50, 50);
+                painter.drawText(rectangle, Qt::AlignCenter, QString("%1").arg(n->valeur));     //Dessine le numero du sommet dans le cerlce
                 break;
             }
 
              case 2: {      //Profondeur 2
 
-                if(cpt2 == 1)
+                if(cpt2 == 1)   //Fils droite
                 {
-                    if(n->valeur == 22222)
+                    if(n->valeur == 22222) //Si on a un sommet fantome
                     {
-                        etage_2 += 200;
+                        etage_2 += 275;
                     }
                     else
                     {
-                    painter.drawLine(225,100,etage_2+25,175);
-                    const QRect rectangle = QRect(etage_2, 150, 50, 50);
-                    painter.setBrush(Qt::red);
-                    painter.drawEllipse(etage_2, 150, 50, 50);
-                    painter.drawText(rectangle, Qt::AlignCenter, QString("%1").arg(n->valeur));
+                        painter.drawLine(290,75,etage_2+25,150);
+                        const QRect rectangle = QRect(etage_2, 125, 50, 50);
+                        painter.setBrush(Qt::red);
+                        painter.drawEllipse(etage_2, 125, 50, 50);
+                        painter.drawText(rectangle, Qt::AlignCenter, QString("%1").arg(n->valeur));
 
-                    etage_2 += 200;
+                        etage_2 += 275;
                     }
                 }
-                if(cpt2 == 2)
+                if(cpt2 == 2) //Fils gauche
                 {
                     if(n->valeur == 22222)
                     {
-                        etage_2 += 200;
+                        etage_2 += 275;
                     }
                     else
                     {
-                    painter.drawLine(225,100,etage_2+25,175);
-                    const QRect rectangle = QRect(etage_2, 150, 50, 50);
+                    painter.drawLine(290,75,etage_2+25,150);
+                    const QRect rectangle = QRect(etage_2, 125, 50, 50);
                     painter.setBrush(Qt::red);
-                    painter.drawEllipse(etage_2, 150, 50, 50);
+                    painter.drawEllipse(etage_2, 125, 50, 50);
                     painter.drawText(rectangle, Qt::AlignCenter, QString("%1").arg(n->valeur));
-                    etage_2 += 200;
+                    //etage_2 += 275;
                     }
                 }
                 break;
@@ -108,74 +117,110 @@ void MainWindow::affiche_arbre(ArbreB B)
 
              case 3: {      //Profondeur 3
 
-                if (cpt3 == 1)
+                if ((cpt3 == 1) || (cpt3 == 2))
                 {
                     if(n->valeur == 33333)
                     {
-                        etage_3 += 100;
+                        etage_3 += 150;
                     }
                     else
                     {
-                        painter.drawLine(etage_2+25-400,200,etage_3+25,275);
-                        const QRect rectangle = QRect(etage_3, 250, 50, 50);
+                        painter.drawLine(etage_2-525,175,etage_3+25,250);
+                        const QRect rectangle = QRect(etage_3, 225, 50, 50);
                         painter.setBrush(Qt::red);
-                        painter.drawEllipse(etage_3, 250, 50, 50);
+                        painter.drawEllipse(etage_3, 225, 50, 50);
                         painter.drawText(rectangle, Qt::AlignCenter, QString("%1").arg(n->valeur));
-                        etage_3 += 100;
+                        etage_3 += 150;
                     }
                 }
-                if (cpt3 == 2)
+                if ((cpt3 == 3) || (cpt3 == 4))
                 {
                     if(n->valeur == 33333)
                     {
-                        etage_3 += 100;
+                        etage_3_bis += 150;
                     }
                     else
                     {
-                        painter.drawLine(etage_2+25-400,200,etage_3+25,275);
-                        const QRect rectangle = QRect(etage_3, 250, 50, 50);
+                        painter.drawLine(400+25,175,etage_3_bis+25,250);
+                        const QRect rectangle = QRect(etage_3_bis, 225, 50, 50);
                         painter.setBrush(Qt::red);
-                        painter.drawEllipse(etage_3, 250, 50, 50);
+                        painter.drawEllipse(etage_3_bis, 225, 50, 50);
                         painter.drawText(rectangle, Qt::AlignCenter, QString("%1").arg(n->valeur));
-                        etage_3 += 100;
+                        etage_3_bis += 150;
                     }
-                }
-                if (cpt3 == 3)
-                {
-                    if(n->valeur == 33333)
-                    {
-                        etage_3 += 100;
-                    }
-                    else
-                    {
-                        painter.drawLine(etage_2+25-200,200,etage_3+25,275);
-                        const QRect rectangle = QRect(etage_3, 250, 50, 50);
-                        painter.setBrush(Qt::red);
-                        painter.drawEllipse(etage_3, 250, 50, 50);
-                        painter.drawText(rectangle, Qt::AlignCenter, QString("%1").arg(n->valeur));
-                        etage_3 += 100;
-                    }
-                }
-                if (cpt3 == 4)
-                {
-                    if(n->valeur == 33333)
-                    {
-                        etage_3 += 100;
-                    }
-                    else
-                    {
-                        painter.drawLine(etage_2+25-200,200,etage_3+25,275);
-                        const QRect rectangle = QRect(etage_3, 250, 50, 50);
-                        painter.setBrush(Qt::red);
-                        painter.drawEllipse(etage_3, 250, 50, 50);
-                        painter.drawText(rectangle, Qt::AlignCenter, QString("%1").arg(n->valeur));
-                        etage_3 += 100;
-                    }
-
                 }
                 cpt3++;
                 break;
              }
+             case 4: {      //Profondeur 4
+
+                if ((cpt4 == 1) || (cpt4 == 2))
+                {
+                    if(n->valeur == 44444)
+                    {
+                        etage_4 += 75;
+                    }
+                    else
+                    {
+                        painter.drawLine(etage_3+25-300,275,etage_4+25,325);
+                        const QRect rectangle = QRect(etage_4, 325, 50, 50);
+                        painter.setBrush(Qt::red);
+                        painter.drawEllipse(etage_4, 325, 50, 50);
+                        painter.drawText(rectangle, Qt::AlignCenter, QString("%1").arg(n->valeur));
+                        etage_4 += 75;
+                    }
+                }
+                if ((cpt4 == 3) || (cpt4 == 4))
+                {
+                    if(n->valeur == 44444)
+                    {
+                        etage_4_bis += 75;
+                    }
+                    else
+                    {
+                        painter.drawLine(etage_3+25-150,275,etage_4_bis+25,325);
+                        const QRect rectangle = QRect(etage_4_bis, 325, 50, 50);
+                        painter.setBrush(Qt::red);
+                        painter.drawEllipse(etage_4_bis, 325, 50, 50);
+                        painter.drawText(rectangle, Qt::AlignCenter, QString("%1").arg(n->valeur));
+                        etage_4_bis += 75;
+                    }
+                }
+                if ((cpt4 == 5) || (cpt4 == 6))
+                {
+                    if(n->valeur == 44444)
+                    {
+                        etage_4_bis_bis += 75;
+                    }
+                    else
+                    {
+                        painter.drawLine(etage_3_bis+25-300,275,etage_4_bis_bis+25,325);
+                        const QRect rectangle = QRect(etage_4_bis_bis, 325, 50, 50);
+                        painter.setBrush(Qt::red);
+                        painter.drawEllipse(etage_4_bis_bis, 325, 50, 50);
+                        painter.drawText(rectangle, Qt::AlignCenter, QString("%1").arg(n->valeur));
+                        etage_4_bis_bis += 75;
+                    }
+                }
+                if ((cpt4 == 7) || (cpt4 == 8))
+                {
+                    if(n->valeur == 44444)
+                    {
+                        etage_4_bis_bis_bis += 75;
+                    }
+                    else
+                    {
+                        painter.drawLine(etage_3_bis+25-150,275,etage_4_bis_bis_bis+25,325);
+                        const QRect rectangle = QRect(etage_4_bis_bis_bis, 325, 50, 50);
+                        painter.setBrush(Qt::red);
+                        painter.drawEllipse(etage_4_bis_bis_bis, 325, 50, 50);
+                        painter.drawText(rectangle, Qt::AlignCenter, QString("%1").arg(n->valeur));
+                        etage_4_bis_bis_bis += 75;
+                    }
+                }
+                cpt4++;
+                break;
+            }
         }
         if (n->fils_gauche != NULL) fifo.push(n->fils_gauche);       // Ajout du fils gauche s'il existe
 
@@ -196,19 +241,20 @@ void MainWindow::affiche_arbre(ArbreB B)
                 }
                 if(getLevel(B.racine, n->valeur) == 3)
                 {
-                    vide->valeur = 33333;       //Valeur aberante pour profondeur 3;
-                    vide2->valeur = 33333;
+                    vide->valeur = 44444;       //Valeur aberante pour profondeur 3;
+                    vide2->valeur = 44444;
                 }
                 if(getLevel(B.racine, n->valeur) == 4)
                 {
-                    vide->valeur = 44444;       //Valeur aberante pour profondeur 4;
-                    vide2->valeur = 44444;
+                    vide->valeur = 55555;       //Valeur aberante pour profondeur 4;
+                    vide2->valeur = 55555;
                 }
                 fifo.push(vide);
                 fifo.push(vide2);
             }
         }
-        if(((n->fils_droite == NULL) && (n->fils_gauche != NULL)) || ((n->fils_droite != NULL) && (n->fils_gauche == NULL)))    //SI on a 1 fils Creation d'un sommet fantom pour conserver l'espacement
+        //SI on a 1 fils Creation d'un sommet fantome pour conserver l'espacement
+        if(((n->fils_droite == NULL) && (n->fils_gauche != NULL)) || ((n->fils_droite != NULL) && (n->fils_gauche == NULL)))
         {
             Sommet * vide = new Sommet;     //Sommet fantome
             vide->fils_droite = NULL;
@@ -244,19 +290,26 @@ void MainWindow::affiche_arbre(ArbreB B)
 void MainWindow::paintEvent(QPaintEvent *event)
 {
 
-    setFixedSize(500, 500);
+    setFixedSize(600, 600);
 
 
     ArbreB B;
     B.Ajouter(5);
     B.Ajouter(8);
     B.Ajouter(2);
-    B.Ajouter(1);
+    B.Ajouter(0);
     B.Ajouter(10);
     B.Ajouter(6);
+    B.Ajouter(3);
+    B.Ajouter(4);
+    B.Ajouter(7);
+    B.Ajouter(1);
+    B.Ajouter(9);
+    B.Ajouter(11);
+    B.Ajouter(-1);
     B.Affiche(B.racine,0);
 
-    //affiche_arbre(B);
+   // affiche_arbre(B);
 
     Sommet * ptr = B.Recherche(1);
 
